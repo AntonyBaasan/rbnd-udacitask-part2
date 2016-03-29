c0b5ee37a3642a3bc12111f751c170e4b0ec34b2
Ankhbayar Baasandorj <antony.baasan@gmail.com>
2016-03-28 22:32:44 -0600

    complete listable

--- lib/listable.rb
+++ lib/listable.rb
@@ -5,4 +5,23 @@ module Listable
     "#{description}".ljust(30)
   end
   
+  def format_due_date (due)
+    due ? due.strftime("%D") : "No due date"
+  end
+
+  def format_duration_date (start_date, end_date)
+    dates = start_date.strftime("%D") if start_date
+    dates << " -- " + end_date.strftime("%D") if end_date
+    dates = "N/A" if !dates
+    return dates
+  end
+
+  def format_priority priority
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
+
 end
